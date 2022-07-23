6644bcc743b5377282a159e5dc464e15e1ec603f
Flashbrv <belnitskiy@i.ua>
2022-07-23 15:58:50 +0300

    init commit

---
+++ src/main/java/com/example/studentapp/SrudentappApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrudentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrudentappApplication.class, args);
+	}
+
+}
